1cdf540375224e98574cf98e8c6c9d1eafd88b7b
Sandip Das <asksandipd@gmail.com>
2018-02-14 20:33:56 +0530

    Updated creds before deploy to Heroku

--- creds.py
+++ creds.py
@@ -1,9 +1,9 @@
 #Alexa
-Security_Profile_Description =""
-Security_Profile_ID  = ""
-Client_ID = ""
-Client_Secret = ""
-Product_ID = ""
+Security_Profile_Description ="SecProfile For AlexaWeb"
+Security_Profile_ID  = "amzn1.application.17280668cc794e2a96327da812dda041"
+Client_ID = "amzn1.application-oa2-client.d9d8f46336114ac9be287e8433754197"
+Client_Secret = "431ae22917a8b2b39f640dfb1fbcf967f25dc806108dfca6d7f206ab1d1757d7"
+Product_ID = "Alexawebsandy"
 
 #Redis
-redis_url = ""
+redis_url = "redis://h:p3332f8f64ccdde0582974b32c4da1870a563cc7b1829f38ba6aa713ce3cf4faa@ec2-54-82-94-125.compute-1.amazonaws.com:65409"
